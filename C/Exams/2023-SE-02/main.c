03229e6209186e33bdcb292df379270fac3ab9b2
Kristina Ganova <115446275+kristinaganova@users.noreply.github.com>
2025-06-01 22:28:38 +0300

    Update main.c

--- C/Exams/2023-SE-02/main.c
+++ C/Exams/2023-SE-02/main.c
@@ -1,98 +1,118 @@
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <err.h>
+//for the bonus task we can create our own idx file by itterating through the 
+//dictionary file and writing the offsets of each word to the new idx file and
+//after that the code remains the same
+
 #include <stdint.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <err.h>
 
 int openFile(const char* path, int flags, mode_t mode) {
-    if ( path == NULL ) errx(2, "Invalid path");
+    if ( path == NULL ) { errx(2, "Invalid path"); }
 
     int fd;
-    if ( (fd = open(path, flags, mode)) == -1) {
-        err(3, "Errro while opening file");
+    if ( (fd = open(path, flags, mode)) == -1 ) {
+        err(3, "Error whiel opening file %s", path);
     }
+
     return fd;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        errx(1, "Expected 2 arguments");
-    }
-
-    int fd_messages = openFile(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0644);
-    int fd_stream = openFile(argv[1], O_RDONLY, 0);
+off_t getFileSize(int fd) {
+    if (fd < 3) { errx(4, "Invalid file descriptor"); }
 
     struct stat st;
-    if ( fstat(fd_stream, &st) == -1 ) {
-        err(9, "Error while stating");
-    }
-
-    uint8_t byte;
-    int readBytes;
-    uint8_t checksum;
-    while ( (readBytes = read(fd_stream, &byte, sizeof(uint8_t))) > 0) {
-        if ( byte != 0x55 ) continue;
-        
-        checksum = byte;
-
-        uint8_t N;
-        if ( read(fd_stream, &N, sizeof(uint8_t)) == -1 ) {
-            err(4, "Error while reading");
-        }   
-        checksum ^= N;
-
-        int currentPos;
-
-        if ((currentPos = lseek(fd_stream, 0, SEEK_CUR)) == -1 ) {
-            err(10, "Error while lseeking");
-        }
-
-        if (currentPos + (N - 2) > st.st_size) {
-            continue;
-        }
-
-        //uint8_t* arr = malloc(N * sizeof(uint8_t)); 
-        uint8_t arr[1024]; // Assuming N will not exceed 1024
-        //if ( arr == NULL ) errx(8, "Memory allocation error");
-        arr[0] = byte;
-        arr[1] = N;
-
-        for ( int i = 2; i < N - 1; i++ ) {
-            if ( read(fd_stream, &arr[i], sizeof(uint8_t)) == -1 ) {
-                //free(arr);
-                err(4, "Error while reading");
-            }
-            checksum ^= arr[i];
-        }
+    if (fstat(fd, &st) == -1) { err(5, "Error while fstating"); }
     
-        uint8_t cs;
-        if ( read(fd_stream, &cs, sizeof(uint8_t)) == -1 ) {
-            //free(arr);
-            err(4, "Error while reading");
-        }
-
-        if (cs != checksum) {
-            //free(arr);
-            continue;
-        }
-         
-        arr[N-1] = cs;
-
-        if (write(fd_messages, arr, sizeof(uint8_t) * N) == -1) {
-            //free(arr);
-            err(6, "Error whil writing");
-        }
-        //free(arr);
-    }
-
-    if ( readBytes == -1 ) {
-        err(4, "Error while reading");
-    }
-
-    close(fd_messages);
-    close(fd_stream);
-
-    return 0;
+    return st.st_size;
 }
- 
+
+void lseekSafe(int fd, int pos, int whence) {
+    if (fd < 3) { errx(4, "Invalid file descriptor"); }
+
+    if (lseek(fd, pos, whence) == -1 ) {
+        err(6, "Error while lseeking");
+    }
+}
+
+int main(int argc, char* argv[]) {
+	if ( argc != 4 ) {
+        errx(1, "Expected 3 arguments");
+	}
+
+	const char* searched = argv[1];
+
+	int fd_dic = openFile(argv[2], O_RDONLY, 0);
+	int fd_idx = openFile(argv[3], O_RDONLY, 0);
+
+    off_t dicSize = getFileSize(fd_dic);
+    off_t idxSize = getFileSize(fd_idx);
+    int left = 0;
+    int right = idxSize / sizeof(uint32_t);
+
+    while (left < right) {
+        int mid = left + (right - left)/2;
+        lseekSafe(fd_idx, mid * sizeof(uint32_t), SEEK_SET);
+
+        uint32_t offset;
+        if ( read(fd_idx, &offset, sizeof(uint32_t)) == -1 ) {
+            err(7, "Errro while reading offset");
+        }
+
+        if ( dicSize < offset ) { errx(8, "Invalid offset"); }
+
+        lseekSafe(fd_dic, offset + 1, SEEK_SET);
+
+        char word[64];
+        int readBytes;
+        int idx = 0;
+        while ( (readBytes = read(fd_dic, &word[idx], sizeof(char))) > 0) {
+            if (word[idx] == '\n' ) {
+                word[idx] = '\0';
+                break;
+            }         
+            idx++;
+        }
+        if ( readBytes == -1 ) {
+            err(7, "Errro while reading word" );
+        }
+
+        int res = strcmp(word, searched);
+        if (res == 0 ) {
+            char c;
+            while( (readBytes = read(fd_dic, &c, sizeof(char))) > 0 ) {
+                if ( c == '\0' ) {
+                    break;
+                }
+
+                if ( write(1, &c, sizeof(c)) == -1) {
+                    err(8, "Errro while writing");
+                }
+            }
+            if ( readBytes == -1 ) {
+                err(7, "Errro while reading word" );
+            }
+
+            if(write(1, "\n", sizeof(c)) == -1) {
+				err(6, "can't write");
+			}
+
+			close(fd_dic);
+			close(fd_idx);
+            exit(0);
+        } else if (res > 0) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+	close(fd_dic);
+	close(fd_idx);
+
+	errx(1, "Word %s has no definition", searched);
+
+}
